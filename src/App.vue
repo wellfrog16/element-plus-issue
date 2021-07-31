936a9575630166319c1de1db2e2bdb1f86b347a7
wellfrog <wellfrog16@gmail.com>
2021-07-31 19:46:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-button>测试一下</el-button>
+  <el-form>
+    <el-input></el-input>
+    <el-input></el-input>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css'
+import App from './App.vue'
+
+import './index.scss'
+
+const app = createApp(App)
+app.use(ElementPlus, { size: 'medium', zIndex: 800 });
+
+app.mount('#app')
